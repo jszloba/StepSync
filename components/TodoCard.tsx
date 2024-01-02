2bb8954068ca325f163412a4907c2a2c11c4142d
Vladimir <V.Arutyunov@russianpost.ru>
2024-01-03 00:57:49 +0400

    feat(TodoCard): Добавлен компонент

---
+++ components/TodoCard.tsx
@@ -0,0 +1,42 @@
+import {DraggableProvidedDraggableProps, DraggableProvidedDragHandleProps} from "react-beautiful-dnd";
+import React from "react";
+import {XCircleIcon} from "@heroicons/react/16/solid";
+
+type Props = {
+    todo: Todo;
+    index: number;
+    id: TypedColumn;
+    innerRef: (element: HTMLElement | null) => void;
+    draggableProps: DraggableProvidedDraggableProps;
+    dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+
+}
+export const TodoCard: React.FC<Props> = ({
+    todo,
+    index,
+    id,
+    innerRef,
+    dragHandleProps,
+    draggableProps
+}) => {
+    return (
+        <div
+            className="bg-white rounded-md space-y-2 drop-shadow-md"
+            {...draggableProps}
+            {...dragHandleProps}
+            ref={innerRef}
+        >
+
+            <div className="flex justify-between items-center p-5">
+                <p>{todo.title}</p>
+                <button className="text-red-500 hover:text-red-600">
+                    <XCircleIcon
+                        className="ml-5 h-8 w-8"
+                    />
+                </button>
+            </div>
+
+
+        </div>
+    );
+};
